e68e39a2141c8a61083078ac7529a3db50ed8e12
biboy7777 <biboyilano777@gmail.com>
2025-05-14 22:52:54 +0800

    scuffed user profile
    fix css, formatting, layout

---
+++ parent/userProfile/userProfile.js
@@ -0,0 +1,15 @@
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password');
+    const hideIcon = document.querySelector('.password-toggle .hide-icon'); // eye-slash SVG
+    const showIcon = document.querySelector('.password-toggle .show-icon'); // eye SVG
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        hideIcon.style.display = 'none';
+        showIcon.style.display = 'block';
+    } else {
+        passwordInput.type = 'password';
+        hideIcon.style.display = 'block'; 
+        showIcon.style.display = 'none';
+    }
+}
