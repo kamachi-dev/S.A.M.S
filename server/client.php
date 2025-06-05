edd63c4d1dfa2620b46709234c0ad28cbd1b3093
Machi <Angelo.kamachi03@gmail.com>
2025-06-05 19:19:18 +0800

    Account generalization

--- server/client.php
+++ server/client.php
@@ -1,12 +1,12 @@
 <?php
     $client = new SoapClient(null, [
-        "location" => "http://localhost:8080/SAMS/server.php",
-        "uri" => "http://localhost:8080/SAMS/server.php"
+        "location" => "http://localhost/SAMS/server.php",
+        "uri" => "http://localhost/SAMS/server.php"
     ]);
 
-    $loginResponse = $client->__soapCall('Login', [[
-        'Username' => 'Kamachi',
-        'Password' => 'root'
+    $loginResponse = $client->__soapCall('LogIn', [[
+        "Email" => '2022ajkamachi@live.mcl.edu.ph',
+        "Password" => 1
     ]]);
 
     $token = $loginResponse['Token'];
@@ -14,9 +14,9 @@
     // Future call: Authenticated with token
     $auth = new stdClass();
     $auth->Token = $token;
-    $header = new SoapHeader('http://localhost:8080/SAMS/server.php', 'AuthHeader', $auth);
+    $header = new SoapHeader('http://localhost/SAMS/server.php', 'AuthHeader', $auth);
 
-    $client->__setSoapHeaders($header);
+   $client->__setSoapHeaders($header);
     $response = $client->__soapCall('GetCurrentUser', []);
     echo "Hello, " . $response;
 ?>
