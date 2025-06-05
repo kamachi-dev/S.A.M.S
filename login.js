f36834cf43df0e076d1188aab136672779e39b65
Homay <154643838+FHOInfante@users.noreply.github.com>
2025-06-06 00:56:36 +0800

    Added Simple Validation for Sign In

--- login.js
+++ login.js
@@ -33,6 +33,43 @@ function ChangeImg() {
         changing = false;
     }, 1500);
 }
+
+document.getElementById("signInBtn").addEventListener("click", function () {
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const usernameError = document.getElementById("usernameError");
+    const passwordError = document.getElementById("passwordError");
+
+    // Clear previous errors
+    usernameError.textContent = "";
+    passwordError.textContent = "";
+
+    if (!username && !password){
+        usernameError.textContent = "Username is required.";
+        passwordError.textContent = "Password is required.";
+        return
+    } 
+
+    else if (!username) {
+        usernameError.textContent = "Username is required.";
+        return;
+    }
+
+    else if (!password) {
+        passwordError.textContent = "Password is required.";
+        return;
+    }
+
+
+    // Apply API here for checking Accounts
+    if (username !== "demo" || password !== "demo") {
+        passwordError.textContent = "Invalid username or password.";
+        return;
+    }
+
+    window.location.href = "Users/parent/viewReport.html";
+});
+
 async function LoopImg() {
     setTimeout(function() {
         imgIndex = imgIndex % 3 + 1;
