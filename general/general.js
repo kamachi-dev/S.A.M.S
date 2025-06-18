eecaddfb03a75f17449cff293d7e44c2a5b02aa0
Arvin Miguel Antonio <irshadow08@gmail.com>
2025-06-18 11:12:19 +0800

    testing dont push

--- general/general.js
+++ general/general.js
@@ -1,49 +1,49 @@
-// const params = window.location.search;
+const params = window.location.search;
 
-// const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.search);
 
-// function getCookie(name) {
-//     return document.cookie
-//         .split('; ')
-//         .find(row => row.startsWith(name + '='))
-//         ?.split('=')[1];
-// }
-// function cookieExists(name) {
-//     return document.cookie.split('; ').some(cookie => cookie.startsWith(name + '='));
-// }
+function getCookie(name) {
+    return document.cookie
+        .split('; ')
+        .find(row => row.startsWith(name + '='))
+        ?.split('=')[1];
+}
+function cookieExists(name) {
+    return document.cookie.split('; ').some(cookie => cookie.startsWith(name + '='));
+}
 
-// function verifyToken(data) {
-//     if (data.hasOwnProperty('credential_error')) {
-//         console.log('session expired, redirecting to login');
-//         window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
-//         return false;
-//     }
-//     return true;
-// }
+function verifyToken(data) {
+    if (data.hasOwnProperty('credential_error')) {
+        console.log('session expired, redirecting to login');
+        window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
+        return false;
+    }
+    return true;
+}
 
-// if (urlParams.has('tkn')/* && urlParams.has('provider')*/) {
-//     document.cookie = `tkn=${urlParams.get('tkn')}; path=/; max-age=10800`;
-//     document.cookie = `provider=${urlParams.get('provider')}; path=/; max-age=10800`;
-// }
-// if (!cookieExists('tkn')/* || !cookieExists('provider')*/) {
-//     window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
-// }
-// window.verifyToken = verifyToken;
-// window.token = getCookie('tkn');
-// window.provider = getCookie('provider');
+if (urlParams.has('tkn')/* && urlParams.has('provider')*/) {
+    document.cookie = `tkn=${urlParams.get('tkn')}; path=/; max-age=10800`;
+    document.cookie = `provider=${urlParams.get('provider')}; path=/; max-age=10800`;
+}
+if (!cookieExists('tkn')/* || !cookieExists('provider')*/) {
+    window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
+}
+window.verifyToken = verifyToken;
+window.token = getCookie('tkn');
+window.provider = getCookie('provider');
 
-// console.log('token:', window.token);
-// console.log('provider:', window.provider);
+console.log('token:', window.token);
+console.log('provider:', window.provider);
 
-// fetch(`https://sams-backend-u79d.onrender.com/getData.php?action=getUserdetails&provider=${window.provider}&tkn=${window.token}`, {
-//     credentials: 'include'
-// })
-//     .then(res => res.json())
-//     .then(json => {
-//         window.email = json['email'];
-//         window.pfp = json['pfp'];
-//     });
+fetch(`https://sams-backend-u79d.onrender.com/getData.php?action=getUserdetails&provider=${window.provider}&tkn=${window.token}`, {
+    credentials: 'include'
+})
+    .then(res => res.json())
+    .then(json => {
+        window.email = json['email'];
+        window.pfp = json['pfp'];
+    });
 
-// const url = new URL(window.location);
-// url.search = '';
-// window.history.replaceState({}, document.title, url.pathname);
+const url = new URL(window.location);
+url.search = '';
+window.history.replaceState({}, document.title, url.pathname);
