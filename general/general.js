5f1f9dcbe44b60f885b8fd99df21b09589ef9e78
biboy7777 <biboyilano777@gmail.com>
2025-07-14 21:55:52 +0800

    Revert "localhost url test"
    This reverts commit 8c81ce8dcdef57f177f282cd77e16c2d2b411cce.

--- general/general.js
+++ general/general.js
@@ -15,8 +15,7 @@ function cookieExists(name) {
 function verifyToken(data) {
     if (data.hasOwnProperty('credential_error')) {
         console.log('session expired, redirecting to login');
-        // window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
-        window.location.href = "https://localhost:8000?error=credential_error";
+        window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
         return false;
     }
     return true;
@@ -27,8 +26,7 @@ if (urlParams.has('tkn')/* && urlParams.has('provider')*/) {
     document.cookie = `provider=${urlParams.get('provider')}; path=/; max-age=10800`;
 }
 if (!cookieExists('tkn')/* || !cookieExists('provider')*/) {
-    // window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
-    window.location.href = "https://localhost:8000?error=credential_error";
+    window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
 }
 window.verifyToken = verifyToken;
 window.token = getCookie('tkn');
@@ -37,9 +35,7 @@ window.provider = getCookie('provider');
 console.log('token:', window.token);
 console.log('provider:', window.provider);
 
-// fetch(`https://sams-backend-u79d.onrender.com/api/getUserdetails.php`
-    
-fetch(`https://localhost:8000/api/getUserdetails.php`, {
+fetch(`https://sams-backend-u79d.onrender.com/api/getUserdetails.php`, {
     credentials: 'include',
     method: 'GET',
     headers: {
