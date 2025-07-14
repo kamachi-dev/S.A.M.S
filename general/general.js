8c81ce8dcdef57f177f282cd77e16c2d2b411cce
biboy7777 <biboyilano777@gmail.com>
2025-07-14 21:54:01 +0800

    localhost url test

--- general/general.js
+++ general/general.js
@@ -15,7 +15,8 @@ function cookieExists(name) {
 function verifyToken(data) {
     if (data.hasOwnProperty('credential_error')) {
         console.log('session expired, redirecting to login');
-        window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
+        // window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
+        window.location.href = "https://localhost:8000?error=credential_error";
         return false;
     }
     return true;
@@ -26,7 +27,8 @@ if (urlParams.has('tkn')/* && urlParams.has('provider')*/) {
     document.cookie = `provider=${urlParams.get('provider')}; path=/; max-age=10800`;
 }
 if (!cookieExists('tkn')/* || !cookieExists('provider')*/) {
-    window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
+    // window.location.href = "https://sams-mmcl.netlify.app?error=credential_error";
+    window.location.href = "https://localhost:8000?error=credential_error";
 }
 window.verifyToken = verifyToken;
 window.token = getCookie('tkn');
@@ -35,7 +37,9 @@ window.provider = getCookie('provider');
 console.log('token:', window.token);
 console.log('provider:', window.provider);
 
-fetch(`https://sams-backend-u79d.onrender.com/api/getUserdetails.php`, {
+// fetch(`https://sams-backend-u79d.onrender.com/api/getUserdetails.php`
+    
+fetch(`https://localhost:8000/api/getUserdetails.php`, {
     credentials: 'include',
     method: 'GET',
     headers: {
