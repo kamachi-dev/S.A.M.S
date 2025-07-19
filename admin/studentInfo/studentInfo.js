90e483ddb36528ed69056323ea1d2e07da56400a
billyjackson7 <billyjacksonn777@gmail.com>
2025-07-19 19:21:20 +0800

    Remove Student Info Page from Admin Section

--- admin/studentInfo/studentInfo.js
+++
@@ -1,7 +0,0 @@
-function TogglePopup() {
-    const popup = document.getElementById("popup");
-    popup.classList.toggle("show");
-}
-
-// Student info functionality will be implemented here
-console.log('Student Info page loaded');
